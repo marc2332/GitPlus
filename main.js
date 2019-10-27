c7238e80f68069e83892f2074aa737574ebcda41
Marc Espín <mespinsanz@gmail.com>
2019-10-27 18:21:33 +0100

    Improvements

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ if(GravitonInfo.version !== "1.2.0") {
   return;
 }
 
-const gitParser = require("./core/index.js").gitParser; //Import Git Repository Parser
-
-let OLD_PARSED = {  modified:[]};
+let OLD_PARSED = {  modified:[] };
 
 const refresh = (REPOSITORY) =>{
-  REPOSITORY.getStatus().then((NEW_PARSED)=>{
+  const LOCAL_REPO = require('simple-git')(REPOSITORY);
+  LOCAL_REPO.status((err,NEW_PARSED)=>{
+    if(err) return;
     OLD_PARSED.modified.map((item)=>{
       if(!NEW_PARSED.modified.includes(item)){ //No need to redisplay on items which still being modified
         const element = document.getElementById((graviton.getCurrentDirectory()+item+"_div").replace(/\\|(\/)|\s/g,""));
@@ -28,7 +28,6 @@ const refresh = (REPOSITORY) =>{
             element.children[0].children[1].style = "";
             element.children[0].children[1].children[0].remove();
           }
-
         }
       }
     })
@@ -48,7 +47,7 @@ const refresh = (REPOSITORY) =>{
           }
       }
     })
-    NEW_PARSED.untracked.map((item)=>{
+    NEW_PARSED.not_added.map((item)=>{
      const element = document.getElementById((graviton.getCurrentDirectory()+item+"_div").replace(/\\|(\/)|\s/g,""));
        if(element!=undefined && element.getAttribute("gitted")!=="true"){
          element.setAttribute("gitted","true");
@@ -85,15 +84,12 @@ const refresh = (REPOSITORY) =>{
 }
 
 document.addEventListener("loaded_project",()=>{
-  const CURRENT_REPO = new gitParser({
-    path:graviton.getCurrentDirectory()
-  })
-  refresh(CURRENT_REPO)
+  refresh(graviton.getCurrentDirectory())
   document.addEventListener("file_saved",()=>{
-    refresh(CURRENT_REPO)
+    refresh(graviton.getCurrentDirectory())
   })
   document.addEventListener("load_explorer",()=>{
-    refresh(CURRENT_REPO)
+    refresh(graviton.getCurrentDirectory())
   })
 })
 
@@ -107,26 +103,40 @@ PitPlus.setList({
   "list":{
      "Commit":{
         click: () => {
-          const my_dialog = new Dialog({
-              id:'my_dialog1',
-              title:'Commit message',
-              content:`<input  class="section-1 input2" placeHolder="A commit..." id="commit_label"></input>`,
-              buttons: {
-                  'Continue':{
-                    click:()=>{
-                      const my_repo = new gitParser({
-                        path:graviton.getCurrentDirectory()
-                      })
-                      const { exec } = require("child_process");
-                      exec(`cd ${graviton.getCurrentDirectory()} && git add * && git commit -m ${document.getElementById("commit_label").value} `, (a,content,b) => {
-                        refresh(my_repo)
-                        Explorer.load(graviton.getCurrentDirectory(),'g_directories',true)
-                      })
+          const repo = require('simple-git')(graviton.getCurrentDirectory())
+          repo.branch((err,data)=>{
+            console.log(err,data)
+            const my_dialog = new Dialog({
+                id:'my_dialog1',
+                title:'Commit message',
+                content:`
+                  <h5>Branch:</h5>
+                  <select id="branches_list">
+                    ${data.all.map((branch)=>{
+                      return `<option>${branch}</option>`
+                    })}
+                  </select>
+                  <h5>Message:</h5>
+                  <input class="section-1 input2" placeHolder="A commit..." id="commit_label"></input>
+                `,
+                buttons: {
+                    'Continue':{
+                      click:()=>{
+                        const MESSAGE = document.getElementById("commit_label").value;
+                        const BRANCHES = document.getElementById("branches_list").value
+                        repo.checkout(BRANCHES,()=>{
+                          repo.commit(MESSAGE,()=>{
+                            refresh(graviton.getCurrentDirectory())
+                            Explorer.load(graviton.getCurrentDirectory(),'g_directories',true)
+                          })
+                        })
+
+                      }
                     }
-                  }
-              }
+                }
+            })
+            document.getElementById("commit_label").focus();
           })
-          document.getElementById("commit_label").focus();
         }
       }
   }
